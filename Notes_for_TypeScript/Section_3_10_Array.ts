4a3be57b0430ea5aa407418369fa5900a82ea6bf
Anderson Martins <60161033+BlackCode7@users.noreply.github.com>
2021-11-03 21:56:11 -0300

    Avançando com os estudos de typescript

---
+++ Notes_for_TypeScript/Section_3_10_Array.ts
@@ -0,0 +1,31 @@
+// Section 3.10: Array
+// An array of values:
+// Uma matriz de valores:
+
+let ThreePigs: number[] = [1, 2, 3, 4, 5];
+let genericStringArray: Array<string> = ['first', 'second', 'third'];
+console.log(this.threePigs, genericStringArray);
+
+let teste1: number[] = [1, 2, 3, 4, 5, 6, 7];
+let teste2Array: Array<string> = ['1', '2', '3', '4', '5', '6', '7'];
+console.log(teste1, teste2Array);
+
+let teste3: number[] = [1, 2, 3, 4, 5,6,7];
+let teste3Array: Array<string> = ['1', '2', '3','4', '5', '6', '7'];
+console.log(teste3, teste3Array);
+
+let teste4: number[] = [1, 2, 3, 4, 5, 6,12, 32, 122];
+let teste4Array: Array<string> = ['1', '2', '3','234', '3232', '54'];
+console.log(teste4, teste4Array);
+
+let teste5: number[] = [1, 2, 3, 4, 5, 5653463, 909, 32];
+let teste5Array: Array<string> = ['21', '121', '234', '98098'];
+console.log(teste5, teste5Array);
+
+let teste6: number[] = [1, 2, 3, 4, 5, 234, 989, 676, 9879];
+let teste6Array: Array<string> = ['987643982', '8987', '998'];
+console.log(teste6, teste6Array);
+
+let teste7: number[] = [1, 897897, 56534, 987987, 445.786];
+let teste7Array: Array<string> = ['123', 'lkjh', '876', 'casa'];
+console.log(teste7, teste7Array);
---
+++ Notes_for_TypeScript/Section_3_11_Enum.ts
@@ -0,0 +1,18 @@
+// A type to name a set of numeric values:
+// Um tipo para nomear um conjunto de valores numéricos:
+// Number values default to 0:
+
+enum Day{Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday};
+let bestday: Day = Day.Sunday;
+let bestDay1: Day = Day.Monday;
+let bestDay2: Day = Day.Tuesday;
+let bestDay3: Day = Day.Wednesday;
+let bestDay4: Day = Day.Thursday;
+let bestday5: Day = Day.Friday;
+let bestDay6: Day = Day.Saturday;
+
+//Set a default starting number:
+enum TenPlus { Ten = 10, Eleven, Twelve }
+enum TenPlus1 { Ten = 11, twenty, thirty, forty}
+enum TenPlus2 { Ten = 12, twenty_one, thirty_one, forty_one}
+enum TenPlus3 { Ten = 13, twenty_two,  thirty_two, forty_two}
