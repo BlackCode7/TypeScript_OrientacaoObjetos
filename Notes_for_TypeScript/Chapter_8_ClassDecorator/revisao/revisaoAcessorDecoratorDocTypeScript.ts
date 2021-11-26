189855f56b090c56efccf86b50f6ebb0272d8adb
Anderson Martins <60161033+BlackCode7@users.noreply.github.com>
2021-11-26 19:02:11 -0300

    Add files via upload

---
+++ Notes_for_TypeScript/Chapter_8_ClassDecorator/revisao/revisaoAcessorDecoratorDocTypeScript.ts
@@ -0,0 +1,26 @@
+function configurable(value: boolean){
+    return function(target:any, propertyKey: string, descriptor: PropertyDescriptor){
+        descriptor.configurable = value;
+    };
+}
+
+class Point{
+    private _x: number;
+    private _y: number;
+    constructor(x: number, y: number){
+        this._x = x;
+        this._y = y;
+    }
+
+    @configurable(false)
+    get x(){
+        return this._x;
+    }
+    @configurable(false)
+    get y(){
+        return this._y;
+    }
+}
+
+let tes = new Point(21, 333);
+console.log(tes)
